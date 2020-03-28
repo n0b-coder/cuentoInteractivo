b65f8fa99aa97833c31a4dab9ab72c2ca5c88aab
LauraC-art <61721258+LauraC-art@users.noreply.github.com>
2020-03-28 10:15:51 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-/*const { defaultView } = document;
-const { documentElement } = document;
-const handler = evt => requestAnimationFrame(() => {
-  const hitBottom = (() => (defaultView.innerHeight + defaultView.pageYOffset) >= documentElement.offsetHeight)();
-  hitBottom
-    ? alert("bottom")
-    : console.log('nope')
-});
-document.addEventListener('scroll', handler);
-*/
+var images=['IMGS/I1','IMGS/I2','IMGS/I3'];
+var c=2;
+
+function nextImg(){
+	if(c<3){
+		c++;
+	}else{
+		c=3;
+	}
+	box.innerHTML="<img src="+images[c-1]+".jpg>";
+};
+function prevImg(){
+	if(c>1){
+		c--;
+	}else {
+		c=1;
+	}
+	box.innerHTML="<img src="+images[c-1]+".jpg>";
+};
+var back=true;
+function back(){
+	c=2;
+};
+
+
 $(document).ready(function(){
 	$('.secciones section').hide();
 	$('.secciones section:first').show();
