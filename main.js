579578afb4c5d8fac4eb3eba7fda93086144594c
LauraC-art <u1201955@unimilitar.edu.co>
2020-03-29 20:38:44 -0500

    Elemento de vida acomodado con css en la pantalla de pilar (Indagar resolver)
    Se ha acomodado el elemento de imagen 'vida'.

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+var pilar1 = {
+	indagacion: ['IMGS/I1.jpg','IMGS/I2.jpg','IMGS/I3.jpg'],
+	textoIndagacion: "arofuitnaroifunt oiarufnt ioarunf toiarnf",
+	background: "url('IMGS/p4.jpg')"
+}
+
+var pilar2 = {
+	indagacion: ['IMGS/p1.jpg','IMGS/p2.jpg','IMGS/p3.jpg'],
+	textoIndagacion: "info del pilar 2",
+	acertijoStyle:{
+		background: "url('IMGS/p5.jpg')"
+	}
+}
+
+var pilares = [pilar1, pilar2];
+var app = new Vue({ 
+  el: '#app',
+  data: {
+  	pilar: pilar1,
+  	idx: 1,
+  	section: 'page'
+  },
+  methods: {
+    activar: function (seccion) {
+      this.section = seccion;
+    },
+    activarPilar: function (numPilar) {
+      this.section = 'acertijo';
+      this.pilar = pilares[numPilar]
+    }
+  }
+});
+
+function nextImg2(){
+	var num = app.idx;
+	num += 1;
+	if (num > 2) num = 2;
+	app.idx = num;
+}
+function prevImg2(){
+	var num = app.idx;
+	num -= 1;
+	if (num < 0) num = 0;
+	app.idx = num;
+}
+
+var back=true;
+function back2(){
+	this.section = 'acertijo';
+	app.section='acertijo'
+	app.idx=1;
+};
+/*
 var images=['IMGS/I1','IMGS/I2','IMGS/I3'];
 var c=2;
 
@@ -34,3 +87,4 @@ $(document).ready(function(){
 		return false;
 	});
 });
+*/
