5ca3925865bd801f5bcebbfd393aa5f632467c9c
LauraC-art <u1201955@unimilitar.edu.co>
2020-03-30 16:27:52 -0500

    Ajuste estilo y html de la sección indagar y resolver.

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 var pilar1 = {
 	indagacion: ['IMGS/I1.jpg','IMGS/I2.jpg','IMGS/I3.jpg'],
-	textoIndagacion: "arofuitnaroifunt oiarufnt ioarunf toiarnf",
+	pilarStyle:	"background: url('IMGS/His2.jpg'); background-size: cover"
 }
 var pilar2 = {
 	indagacion: ['IMGS/p1.jpg','IMGS/p2.jpg','IMGS/p3.jpg'],
-	textoIndagacion: "info del pilar 2",
+	pilarStyle:	"background: url('IMGS/p5.jpg'); background-size: cover"
 }
 var page1 = {
-	pageTxt: "Info chévere de la página 1 :v",
+	pageTxt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 }
 var page2 = {
-	pageTxt: "Info chévere de la página 2 :v",
+	pageTxt: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
 }
 var page3 = {
-	pageTxt: "Info chévere de la página 3 :v",
+	pageTxt: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
 }
 var vida={
 	stat:['IMGS/Vidas/full.jpg','IMGS/Vidas/bar2.jpg','IMGS/Vidas/bar1.jpg','IMGS/Vidas/dead.jpg']
@@ -26,6 +26,7 @@ var app = new Vue({
   	pilar: pilar1,
   	pagina:page1,
   	vida,
+  	estado:2,
   	idx: 1,
   	section:'page1',
   },
@@ -40,11 +41,9 @@ var app = new Vue({
     }
   }
 });
-var num = app.idx; var respuesta=true;
+var num = app.idx;
 function resolver(){
-	if(respuesta==true){
-		vida.stat[0];
-	}	
+	app.estado=3;
 }
 function nextImg2(){
 	num += 1;
@@ -52,7 +51,6 @@ function nextImg2(){
 	app.idx = num;
 }
 function prevImg2(){
-
 	num -= 1;
 	if (num < 0) num = 0;
 	app.idx = num;
