606738a69f07f47644939f7e2249470752c5b5a5
LauraC-art <u1201955@unimilitar.edu.co>
2020-03-31 12:17:48 -0500

    Reajustes en el código de CSS, Js y el html.

--- main.js
+++ main.js
@@ -8,12 +8,15 @@ var pilar2 = {
 }
 var page1 = {
 	pageTxt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	pageBg:	"background: url('IMGS/His1.jpg') no-repeat center center fixed;"
 }
 var page2 = {
 	pageTxt: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
+	pageBg:	"background: url('IMGS/His2.jpg') no-repeat center center fixed;"
 }
 var page3 = {
 	pageTxt: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+	pageBg:	"background: url('IMGS/p3.jpg') no-repeat center center fixed;"
 }
 var vida={
 	stat:['IMGS/Vidas/full.png','IMGS/Vidas/bar2.png','IMGS/Vidas/bar1.png','IMGS/Vidas/dead.png']
