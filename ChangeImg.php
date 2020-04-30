eee8cfc7dee05c9d74615db4f842a8fde2bdb661
SergioDDiaz <61720992+SergioDDiaz@users.noreply.github.com>
2020-04-29 19:19:57 -0500

    cambio de imagen css

---
+++ ChangeImg.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+session_start(); //inicia una sesion o reanuda una existente
+$variable_S =  $_SESSION['user'];
+if($variable_S == null || $variable_S == '')
+{
+    echo "<p class='error'>- por favor inicie sesion para poder ingresar al panel de administracion </p>";
+    die();
+}
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JUKO_IMG_SELECTOR</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta content="JUKO">
+</head>
+<body>
+<div class="Window">
+    <div class="BlockImg">
+        <div class="ShowImg">
+            <img class = "PreImg" src="https://gameranx.com/wp-content/uploads/2016/05/DOOM4-2.jpg">
+        </div>
+        <div class="ShowDat">
+            <input type="text" class="form-control" placeholder="Nombre">
+            <div class="Datos">
+            <h2 id="h2Tipo"> <?php echo $_SESSION['user']?> </h2>
+            <h2 id="h2Secc"> <?php echo $_SESSION['user']?> </h2>
+            <h2 id="h2Pag"> <?php echo $_SESSION['user']?> </h2>
+            </div>
+            <button class="Btn">
+                Cambiar Archivos
+            </button>
+        </div>
+    </div>
+    <div class="ScrollImg">
+        <h1 id="h1Bib"> Biblioteca de imágenes </h1>
+        <div class="BarScroll">
+            <div class="scrolling-wrapper">
+                <div class="cards"> <img class="images" src="https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg"></div>
+                <div class="cards"> <img class="images" src="https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg"></div>
+                <div class="cards"> <img class="images" src="https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg"></div>
+                <div class="cards"> <img class="images" src="https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg"></div>
+                <div class="cards"> <img class="images" src="https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg"></div>
+                <div class="cards"> <img class="images" src="https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg"></div>
+            </div>
+            <button class="AddBtn">
+                +
+            </button>
+        </div>
+    </div>
+</div>
+</body>
+</html>
