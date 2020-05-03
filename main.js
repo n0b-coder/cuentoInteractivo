21f26908a31ef206c1b60e4af1c942098e049c87
LauraC-art <u1201955@unimilitar.edu.co>
2020-05-03 12:16:13 -0500

    Js organizado.

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
-//Info de cada pilar
-
-//vidas
+//finales definidos dentro del Js
 var finales = {
-	//
 	reset:["JS Has fracasado","JS Has descubierto el mejor final posible","JS Has descubierto el final secundario de la historia", ],
 }
+//Vidas desde la antigua carpeta IMGS
 var vida={
 	stat:['IMGS/Vidas/full.png','IMGS/Vidas/bar2.png','IMGS/Vidas/bar1.png','IMGS/Vidas/dead.png']
 }
-//torre de cada pilar
-var torre = {
-	torreBg:"background: url('IMGS/Fres.png') no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover;"
-}
-//Array de pilares
-//Contraseñas :O
-var _0xfd70=["\x61\x62\x63\x64","\x65\x66\x67\x68","\x69\x6A\x6B\x6C"];
-eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('5 4=[3[0],3[1],3[2]]',6,6,'|||_0xfd70|validos|var'.split('|'),0,{}))
-//___
-
-//var portada = {"texto":"texto portada"}
-
-
+//
 var game_data = {
 	"historia":[
 		[
@@ -39,9 +25,7 @@ var app = new Vue({
 	pilSect:0,
 	pg:0,
 	id:1,
-	total:3*3,
 	vida,
-	torre,
 	alOrDe:"REINTENTAR",
 	resetBtn:"",
 	final:finales,
@@ -53,9 +37,9 @@ var app = new Vue({
   computed: {
     backs: function () {
       	if(this.section=='final'){
-			  var game_data=game_data;
 			return 'background-image:url("'+this.game_data.finales[this.counterf][0].imagen_fondo+'")';
 		} else if(this.section=='reset'){
+			//fslta pantalla de reset en DB
 			return 'background-image:url("'+this.game_data.finales[this.counterf][0].imagen_fondo+'")';
 		} else if(this.section=='resolver'){
 			return 'background-image:url("'+this.game_data.pilares[this.id].fondo_acertijo+'")';
@@ -69,39 +53,103 @@ var app = new Vue({
 		else {			
       		return 'background-image:url("'+this.game_data.historia[this.pilSect][this.pg].imagen_fondo+'")';
 		}
-    },
+	},
 	//imágenes acertijo
-	imagenes(){
+	imagenes: function (){
 		if (this.section=='indagar'){
 			return this.game_data.indagacion[this.pilSect][this.idx].imagen_fondo;
 		}
 		return 'background-image:url("'+this.game_data.pilares[this.id].imagen_acertijo+'")';
 	},
-	monitor(){
+	//Imagen monitor
+	monitor:function (){
 		//return 'background-image:url('+game_data algo con monitor+')';
 	},
-	reset(){
-		var game_data=game_data;
-		if(this.section=='reset'){
-			return 'background-image:url("'+this.game_data.historia[this.counterf][0].imagen_fondo+'")';
-		}
+	//Pantalla redirección a index
+	reset:function (){
+		//return this.game_data.finales[this.counterf][0].texto; algo así pero con reset :V
 	},
-	finalT(){
+	//Texto final
+	finalT:function (){
 		return this.game_data.finales[this.counterf][0].texto;
+	},
+	pass: function(){
+		return this.secret.toLowerCase();
+	},
+	total: function(){
+		return Object.keys(this.game_data.pilares).length;
 	}
   },
   methods: {
+	  //siguiente página
     activar: function (seccion, s) {
       this.section = seccion;
-	  app.pg+=s;
-	  //this.pilar = pilares[app.pilSect];
-    },
+	  this.pg+=s;
+	},
+	//Página Torresss
     activarPilar: function (seccion) {
       this.section = seccion;
-	}
+	},
+	//Resolver acertijo
+	resolver: function (){
+		var validos=this.game_data.pilares[this.id].solucion;
+		if(this.pass==validos){
+			this.pilSect++;
+			this.estado=0;
+			if(this.pilSect==this.total){
+				if(this.total>=Math.round(this.total*3*0.9)){
+					this.counterf=1;
+				}
+				else{
+					this.counterf=2;
+				}
+				this.section='final';
+				this.resetBtn="JUGAR DE NUEVO Y DESCUBRIR MÁS JS";
+			}else{
+				this.section='reintentar';
+				this.alOrDe="¡ENHORABUENA! JS";
+			}
+			
+		}
+		else if(this.pass!=validos){
+			this.estado++;
+			if(this.estado==3){
+				this.counterf=0;
+				this.section='final';	
+				this.resetBtn="INTENTAR DESDE EL PRINCIPIO JS";
+			}else{
+				this.section='reintentar';
+				this.alOrDe="REINTENTAR JS";
+				this.total--;
+			}
+		}
+		this.secret='';
+	},
+	nextImg: function (){
+		this.idx += 1;
+		if (this.idx  > 2) this.idx  = 2;
+	},
+	prevImg: function (){
+		this.idx -= 1;
+		if (this.idx < 0) this.idx = 0;
+	},
+	back: function (){
+		this.section = 'acertijo';
+		this.idx=1;
+	},
+	again: function (){
+		this.section = 'acertijo';
+		if(this.estado==0){//ganó, pasa al sig pilar
+			this.section='page1';
+			this.id++;
+			this.pg=0;
+			s=0;
+			this.idx=1;
+			this.estado=0;
+		}
+	}	
   }
 });
-
 
 const request = new Request('set.php');
 
@@ -120,73 +168,3 @@ fetch(request)
   }).catch(error => {
     console.error(error);
   });
-
-//
-var num = app.idx;
-
-function resolver(){
-	if(app.secret==validos[app.pilSect]){
-		app.pilSect++;
-		app.estado=0;
-		if(app.pilSect==3){
-			if(app.total>=Math.round(3*3*0.6)){
-				app.counterf=1;
-			}
-			else{
-				app.counterf=2;
-			}
-			app.section='final';
-			app.resetBtn="JUGAR DE NUEVO Y DESCUBRIR MÁS JS";
-		}else{
-			app.section='reintentar';
-			app.alOrDe="¡ENHORABUENA! JS";
-		}
-		
-	}
-	else if(this.secret!=validos[app.pilSect]){
-		app.estado++;
-		if(app.estado==3){
-			app.counterf=0;
-			app.section='final';
-
-			app.resetBtn="INTENTAR DESDE EL PRINCIPIO JS";
-		}else{
-			app.section='reintentar';
-			app.alOrDe="REINTENTAR JS";
-			app.total--;
-		}
-	}
-	app.secret='';
-}
-
-
-function nextImg(){
-	num += 1;
-	if (num > 2) num = 2;
-	app.idx = num;
-}
-function prevImg(){
-	num -= 1;
-	if (num < 0) num = 0;
-	app.idx = num;
-}
-
-function back(){
-	this.section = 'acertijo';
-	app.section='acertijo'
-	app.idx=1;
-};
-
-function again(){
-	this.section = 'acertijo';
-	app.section='acertijo'
-	if(app.estado==0){//ganó, pasa al sig pilar
-		this.section='page1';
-		app.section='page1';
-		app.id++;
-		app.pg=0;
-		s=0;
-		app.idx=1;
-		app.estado=0;
-	}
-};
