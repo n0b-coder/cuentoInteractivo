b31834ff317766374b1d27912e9068e12c3d37c1
LauraC-art <u1201955@unimilitar.edu.co>
2020-05-08 15:33:07 -0500

    Update Selector.php

--- Selector.php
+++ Selector.php
@@ -18,23 +18,22 @@
         die();
     }
     require_once("Conexion.php");
-    $sql= "SELECT settings.Id_cuento, cuento.Cuento_Name
+    $sql= "SELECT settings.Id_cuento, cuento.Cuento_Name, settings.Id_personaje
     FROM `settings` JOIN cuento ON  settings.Id_cuento = cuento.Id_cuento";
     $result =  $conn->query($sql);
     if($result->num_rows>0)
     { 
             $row=$result->fetch_assoc();
-            $id_cuento_act = $row['Id_cuento'];
-            $Name_cuento_act = $row['Cuento_Name'];   
+            $id_cuento_sel = $row['Id_cuento'];
+            $Name_cuento_sel = $row['Cuento_Name'];
+            $id_personaje_sel = $row['Id_personaje'];    
     }
-
-
 
 ?>
 
 <body>
 
-    <div class="container-fluid" id="appad">
+    <div class="container-fluid" id="adminApp">
 
         <div class="card-header">
             <div id="Titulodeadmin">
@@ -57,27 +56,30 @@
                         <input type="text" class="form-control" placeholder="Cuento Actual:" readonly>
                         <input type="text" class="form-control" :placeholder="placeName()" readonly>
                 </div>
-            
+                <?php
+                                $selected = $id_cuento_sel;
+                             
+                                ?>
                                
-                    <div class="ScrollImg row">
-                            <div class="scrollingWrapper col">                                    
-                                
+                               <div class="ScrollImg row">
+                            <div class="scrollingWrapper col">
+
                                 <div class="cards" v-for="(item, index) in admin_data.cuentos" :key="index">
-                                <img v-on:click="getId(item[0].id)" :class="{ActBtn:item[0].id == selected , NAddBtn:item[0].id != selected}" @click="selected = item[0].id"  :src="item[0].imagen_fondo">
+                                    <img v-on:click="getId(item.id)" :class="{ActBtn:item.id == selected , NAddBtn:item.id != selected}" @click="selected = item.id"  :src="admin_data.cuentos[0].Portada">
                                 </div>
-                                <div class="cards"> <button class="NAddBtn"> + NEW </button> </div>         
+                                <div class="cards"> <button class="NAddBtn"> + NEW </button> </div>
                             </div>
                     </div>
              
                     <div class= Accion>
                         
                         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
-                            <input type="hidden" name="cuento_id"   value =  "selected">
+                            <input type="hidden" name="cuento_id"   value =  "<?php echo $selected ?>">
                             <input type="submit" value="Actualizar" class="BtnUser" name= "actualize">
                         </form>
 
                         <form action="paneldeadministrador.php" method="post">
-                            <input type="hidden" name="cuento_id"   value =  "selected">
+                            <input type="hidden" name="cuento_id"   value =  "<?php echo $selected ?>">
                             <input type="submit" value="Editar" class="BtnUser" name= "edit">
                         </form>
                     </div>
@@ -93,7 +95,7 @@
 
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 	<script src="https://kit.fontawesome.com/0d8e639741.js" crossorigin="anonymous"></script>
-	<script type="text/javascript" src="main.js"></script>
+	<script type="text/javascript" src="admin.js"></script>
 
 </body>
 </html>
