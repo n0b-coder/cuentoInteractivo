a642ea213e0e30d4385af737ecedce4c9db27dcb
LauraC-art <u1201955@unimilitar.edu.co>
2020-05-25 21:24:04 -0500

    Update paneljs.js

--- paneljs.js
+++ paneljs.js
@@ -160,7 +160,8 @@ var app = new Vue({
       onFileChange(e) {
           var files = e.target.files || e.dataTransfer.files;
           if (!files.length)
-            return this.createImage(files[0]);
+            return;
+          this.createImage(files[0]);
 
           const formData = new FormData();
 
@@ -169,8 +170,8 @@ var app = new Vue({
           formData.append('tipoimagen', this.tipo);
           formData.append('id_imagen', this.id_img);
           formData.append('accion',this.action);
-          updateImg = async () => {
-            const uploaded = await fetch('/upload.php', {
+          
+            const uploaded = fetch('/upload.php', {
               method: 'POST',
               body: formData  
             })
@@ -198,7 +199,7 @@ var app = new Vue({
                     console.error(error);
                   });
             }
-          };
+          
         },
         createImage(file) {
           var reader = new FileReader();
