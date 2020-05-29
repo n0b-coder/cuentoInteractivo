1dfe51bead138e5ca37d16cb5894059bd9f3fbde
LauraC-art <u1201955@unimilitar.edu.co>
2020-05-29 18:32:48 -0500

    Update paneljs.js

--- paneljs.js
+++ paneljs.js
@@ -85,18 +85,6 @@ var app = new Vue({
       setImgs();
     },
     computed:{
-      setPos:function(){
-        switch(this.pos){
-          case 'Centro':
-            return this.pos='center';
-          case 'Derecha':
-          return this.pos='right';
-          case 'Izquierda':
-            return this.pos='left';
-          case 'Posición':
-            return this.panel_data.current_selection.pos_personaje;
-        };
-      },
       photos:function(){
         if(this.active==true){
           if (this.section=='resolucion'){
@@ -213,6 +201,18 @@ var app = new Vue({
             this.panel_data.current_selection.fondo_acertijo = item.Imag_link;
           }
         }
+      },
+      setPos:function(){
+        switch(this.pos){
+          case 'Centro':
+            return this.pos='center';
+          case 'Derecha':
+          return this.pos='right';
+          case 'Izquierda':
+            return this.pos='left';
+          case 'Posición':
+            return this.panel_data.current_selection.pos_personaje;
+        };
       },
       onFileChange(e) {
           var files = e.target.files || e.dataTransfer.files;
